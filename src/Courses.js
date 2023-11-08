c748d5e1b7d6be9cd655ba787aa87ac9a3e95e88
Harish <hari991996@gmail.com>
2023-11-08 19:51:04 +0530

    remove unused var

--- src/Courses.js
+++ src/Courses.js
@@ -1,7 +1,6 @@
 import Course from "./Course";
 import "./Course.css";
 import "./Courses.css";
-import { Link } from "react-router-dom";
 import React from "react";
 import useCourseStore from "./courseStore";
 
